644218d4ceb0f5211f487105e754dac608c503fd
johnnypaid <johnnypaid12@gmail.com>
2022-02-21 20:13:43 +0800

    db and docker setup.

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,29 @@
 import express from 'express';
 import cors from 'cors';
+import { createConnection } from 'typeorm';
 
 const app = express();
 
-// middleware
-app.use(cors());
+// db connection
+createConnection().then(() => {
+    
+    // middleware
+    const corsOption = {
+        origin: 'http://localhost:4200',
+        optionsSuccessStatus: 200
+    }
 
-const port = process.env.PORT || 8000;
+    app.use(cors(corsOption));
+    app.use(express.json());
 
-app.listen(port, () => {
-    console.log(`Server is listening at ${port}`);
+    // routes
+    app.get('/', (req, res) => {
+        res.send('<h2>Ambassador app..</h2>');
+    });
+
+    const port = process.env.PORT || 8000;
+
+    app.listen(port, () => {
+        console.log(`Server is listening at ${port}`);
+    });
 });
